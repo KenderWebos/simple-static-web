a592dd0246c7da9a778361266fe29e04fc83b388
KenderWebos <kenderman.8@gmail.com>
2022-09-22 18:58:40 -0300

    mejora a los 2 navbar

--- view/html/header.php
+++ view/html/header.php
@@ -6,28 +6,61 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> El gran titulo universitario </title>
-    <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/styles.css"> -->
     <link rel="shortcut icon" href="images/tierra_icon.png" href="/favicon.jpeg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
-    <nav class = "main_nav">
-        <ul>
-            <!--a href="index.html"> <img src="view/images/ht_logo.png" alt="heritech_logo"> </a-->
-            <!-- <li><a href=""><img style="width:100px" class="header_company_logo" src="../../../ing_soft/view/images/heritech/ht_logo.png" alt="heritech_main_logo"></a></li> -->
-            <img style="padding:10px;width:200px; height:100%" class="header_company_logo" src="../../../ing_soft/view/images/heritech/ht_logo.png" alt="heritech_main_logo">
-            <li><a href="index.php?p=inicio">HOME</a></li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <!-- <img style="padding:10px;width:200px; height:100%" class="header_company_logo" src="../../../ing_soft/view/images/heritech/ht_logo.png" alt="heritech_main_logo"> -->
+    <a class="navbar-brand" href="index.php?p=inicio"> HeriTech </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tareas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?p=tarea1">Tarea1</a></li>
+            <li><a class="dropdown-item" href="index.php?p=tarea2">Tarea2</a></li>
+            <li><a class="dropdown-item" href="index.php?p=tarea3">Tarea3</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="index.php?p=new_">new</a></li>
+            <li><a class="dropdown-item" href="index.php?p=js_testing">js</a></li>
+            <li><a class="dropdown-item" href="index.php?p=new_">Bootstrap</a></li>
 
-            <li><a href="index.php?p=tarea1">Tarea 1</a></li>
-            <li><a href="index.php?p=Tarea2">Tarea 2</a></li>
-            <li><a href="index.php?p=Tarea3">Tarea 3</a></li>
-            <li><a href="index.php?p=new_">new_</a></li>
-
-            <li><a href="index.php?p=js_testing">JS</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tareas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            
+            <li><hr class="dropdown-divider"></li>
             <li><a href="../../../ing_soft/evaluaciones/test1/test_1/index.html">CINEMARK</a></li>
             <li><a href="../../../ing_soft/view/html/modules/simplepage/index.php">INTERACCIONES</a></li>
+            <li><a href="../../../ing_soft/view/html/modules/bootstrap/index.html">Bootstrap</a></li>
+            <li><hr class="dropdown-divider"></li>
 
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php?p=contactos">Contactos</a>
+        </li>
 
-            <!-- COMO PUEDO PONER RUTAS EXTERNAS COMO LAS 2 ULTIMAS PERO QUE SE CARGEN COMO LAS TAREA 1 2 ETC. -->
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-        </ul>
-    </nav>
+    
