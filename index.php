181e35f31ae0be06e8bff6a378590dad37fd18f8
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-10-04 02:59:57 -0300

    movi el index y partials inicio

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-        <meta name="description" content="la gran descripcion del sitio web">
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> HeriTech </title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="icon" type="image/jpg" href="view/public/images/heritech/logo/heritech_logo_white.png"/>
-        <link rel="stylesheet" href="view/public/css/fix_bootstrap.css">
+    <meta name="description" content="la gran descripcion del sitio web">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> HeriTech </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/jpg" href="view/public/images/heritech/logo/heritech_logo_white.png" />
+    <link rel="stylesheet" href="view/public/css/fix_bootstrap.css">
 </head>
 
+
 <body>
-        <?php
-        //logica para capturar la pagina que queremos abrir
-        $pagina = isset($_GET['p']) ? strtolower($_GET['p']) : 'inicio';
+    <?php
+    //logica para capturar la pagina que queremos abrir
+    $pagina = isset($_GET['p']) ? strtolower($_GET['p']) : 'inicio';
 
-        require_once "view/partials/header.php";
-        require_once "view/partials/" . $pagina . '.php';
-        require_once "view/partials/footer.php";
+    require_once "view/partials/header.php";
+    require_once "view/partials/" . $pagina . '.php';
+    require_once "view/partials/footer.php";
 
-        ?>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <script src="view/public/js/main.js"></script>
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="view/public/js/main.js"></script>
 </body>
+
 </html>
--- view/partials/inicio.php
+++ view/partials/inicio.php
@@ -1,43 +1,45 @@
-<div class="main_page">
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+<div class="carousel-container">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div style="height:500px" class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
         <img src="view/public/images/carousel_main/img_1.png" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
-            <h5>Necesitas una pagina para tu empresa?</h5>
+            <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
         </div>
         </div>
-        <div style="background-image: url('view/public/images/explicit.jpg');" class="carousel-item" data-bs-interval="2000">
-        <img src="view/public/images/carousel_main/img_2.png" class="d-block w-100" alt="...">
+        <div class="carousel-item">
+        <img src="view/public/images/carousel_main/img_1.png" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
             <h5>Second slide label</h5>
             <p>Some representative placeholder content for the second slide.</p>
         </div>
         </div>
         <div class="carousel-item">
-        <img src="view/public/images/carousel_main/img_3.png" class="d-block w-100" alt="...">
+        <img src="view/public/images/carousel_main/img_1.png" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
         </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
     </div>
 </div>
+
+<!-- view/public/images/carousel_main/img_1.png -->
 
 <div class="divplus">
         <center>
